6811587bd7c6ef8342cfde407a30c8db3f03928b
office-version-77331 <officenew601@gmail.com>
2025-01-22 12:07:39 +0530

    actually Now complete With proper context Api

--- Theme App/src/App.jsx
+++ Theme App/src/App.jsx
@@ -1,24 +1,18 @@
-import {  useState } from 'react'
+
 import './App.css'
 import ThemeToggle from './components/ThemeToggle';
 import Content from './components/Content';
-import ThemeContext from './store/ThemeContext';
+import  { ThemeProvider } from './store/ThemeContext';
 
 function App() {
-  const [theme, setTheme] = useState('light')
+  
 
-  const toggleHandler = () => {
-    setTheme(currVal => {
-      console.log('currVal : ', currVal);
-       return currVal === 'light' ? 'dark' : 'light'
-      });
-  }
   return (
    
-    <ThemeContext.Provider value={{theme, toggleHandler}}>
+    <ThemeProvider >
       <ThemeToggle /> 
       <Content /> 
-    </ThemeContext.Provider>
+    </ThemeProvider>
   
   )
 }
--- Theme App/src/store/ThemeContext.jsx
+++ Theme App/src/store/ThemeContext.jsx
@@ -1,5 +1,23 @@
 import React, {createContext} from "react";
+import {  useState } from 'react'
 const ThemeContext = createContext();
+export const  ThemeProvider = ({children}) => {
+    const [theme, setTheme] = useState('light')
+
+    
+  const toggleHandler = () => {
+    setTheme(currVal => {
+      console.log('currVal : ', currVal);
+       return currVal === 'light' ? 'dark' : 'light'
+      });
+  }
+
+  return <ThemeContext.Provider  value={{theme, toggleHandler}}> 
+    {children}
+  </ThemeContext.Provider>
+
+  
+}
 
 
 export default ThemeContext;
